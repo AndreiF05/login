3500a9576e49e7a4a2bbc53d2b66390e4b3f7059
AndreiF05 <AndreiF05@users.noreply.github.com>
2018-12-08 21:28:15 +0200

    ok

---
+++ app/src/main/java/com/example/andrei/app/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.andrei.app;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+        public void register(View view) {
+            Intent intent = new Intent(this, CreateAccount.class);
+            startActivity(intent);
+        }
+
+
+}
