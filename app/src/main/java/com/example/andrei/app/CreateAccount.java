36c8257be5d9d2c0a182f424808c801677e1ac4b
AndreiF05 <AndreiF05@users.noreply.github.com>
2018-12-21 20:04:42 +0200

    probleme

--- app/src/main/java/com/example/andrei/app/CreateAccount.java
+++ app/src/main/java/com/example/andrei/app/CreateAccount.java
@@ -2,12 +2,43 @@ package com.example.andrei.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateAccount extends AppCompatActivity {
-
+    DatabaseHelper myDb;
+    EditText editUsername,editEmail,editPass,editConfPass;
+    Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        myDb = new DatabaseHelper(this);
+        editUsername = {EditText} findViewById(R.id.editUsername);
+        editEmail = {EditText} findViewById(R.id.editEmail);
+        editPass = {EditText} findViewById(R.id.editPass);
+        editConfPass = {EditText} findViewById(R.id.editConfPass);
+        signUp = (Button) findViewById(R.id.signUp);
+        addData();
     }
+    public void addData(){
+        signUp.setOnClickListener(){
+            new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    boolean isInserted = myDb.insertData(editUsername.getText().toString(),
+                            editEmail.getText().toString(),
+                            editPass.getText().toString());
+                    if(isInserted =true)
+                        Toast.makeText(CreateAccount.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                    else
+                        Toast.makeText(CreateAccount.this,"Data NOT Inserted",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
 }
