f0d612a50b426f13849eaec0a7086aa0028b0b68
sbuzoianu <stefan@buzoianu.ro>
2018-12-22 10:58:46 +0200

    adding DatabaseHelper class

--- app/src/main/java/com/example/andrei/app/DatabaseHelper.java
+++ app/src/main/java/com/example/andrei/app/DatabaseHelper.java
@@ -1,4 +1,41 @@
 package com.example.andrei.app;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import java.util.ArrayList;
 
-class DatabaseHelper {
+class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static String DATABASE_NAME = "user_database";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_USER = "users";
+    private static final String KEY_ID = "id";
+    private static final String KEY_FIRSTNAME = "name";
+    private static final String KEY_HOBBY = "hobby";
+
+    private static final String CREATE_TABLE_STUDENTS = "CREATE TABLE "
+            + TABLE_USER + "(" + KEY_ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_FIRSTNAME + " TEXT, "+ KEY_HOBBY + " TEXT );";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_STUDENTS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS '" + TABLE_USER + "'");
+        onCreate(db);
+    }
+
+    public boolean insertData(String s, String s1, String s2) {
+        return true;
+    }
 }
